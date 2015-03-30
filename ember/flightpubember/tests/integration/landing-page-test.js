e7d248450675b6c75f8a87597da26ef30c9148a1
twistyn <tnew2294@gmail.com>
2015-03-30 12:18:17 +1100

    Starting to port things over slowly. A little lost at this point, just in getting everything started

---
+++ ember/flightpubember/tests/integration/landing-page-test.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+
+var App;
+
+module('Integration - Landing Page', {
+    beforeEach: function() {
+        App = startApp();
+    },
+
+    afterEach: function() {
+        Ember.run(App, 'destroy');
+    }
+});
+
+test('Should welcome me to FlightPub', function(assert) {
+    visit('/').then(function() {
+        assert.equal(find('h2#title').text(), 'Welcome to FlightPub');
+    });
+});
