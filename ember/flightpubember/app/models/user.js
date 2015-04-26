acc114507f5652a8e442522250c99a54e5b5bc57
Tristan Newmann <tristan_dev@192-168-1-113.tpgi.com.au>
2015-04-26 13:35:22 +1000

    added a comment

--- ember/flightpubember/app/models/user.js
+++ ember/flightpubember/app/models/user.js
@@ -4,6 +4,6 @@ export default DS.Model.extend({
     email:          DS.attr('string'),
     firstName:      DS.attr('string'),
     lastName:       DS.attr('string'),
-    infoString:            DS.attr('string'), //The infoString is a user bio
+    infoString:     DS.attr('string'), //The infoString is a user bio
     accountStatus:  DS.attr('string')
 });
