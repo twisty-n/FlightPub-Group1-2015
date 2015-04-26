94ab5c239642ee8e77339621e1f5834643275ae3
twistyn <tnew2294@gmail.com>
2015-04-26 15:44:08 +1000

    Change the suer Pkey to make sense

---
+++ rails/db/migrate/20150426053232_fix_user_id_name.rb
@@ -0,0 +1,7 @@
+class FixUserIdName < ActiveRecord::Migration
+  def change
+    # TODO: Note that this will have to be changed when reverting back to MYSQL
+   # execute "ALTER TABLE \"users\" DROP CONSTRAINT users_pk"
+    add_column :users, :id, :primary_key
+  end
+end
