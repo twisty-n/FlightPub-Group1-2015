fdefacbfe287ed19ed7a303f7f915c581baff999
Alistair Woodcock <alistair@imagomedia.com.au>
2015-05-07 22:02:08 +1000

    working on fixtures

--- ember/flightpubember/app/models/flight.js
+++ ember/flightpubember/app/models/flight.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-
+/*
 export default DS.Model.extend({
     flightNumber: DS.attr('string'),
     price:              DS.attr('string'),
@@ -7,3 +7,18 @@ export default DS.Model.extend({
     departureTime:      DS.attr('string'),
     arrivalTime:        DS.attr('string') 
 });
+*/
+
+let flight = DS.Model.extend({
+	flightNumber: 		DS.attr('string'),
+    price:              DS.attr('string'), 
+});
+
+flight.reopenClass({
+	FIXTURES: [
+		{ id: 1, flightNumber: 'A301', price : 999 },
+		{ id: 2, flightNumber: 'A302', price : 910 }
+	]
+});
+
+export default flight;
