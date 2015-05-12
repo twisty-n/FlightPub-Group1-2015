59a338d74f8e9467c837427597894e0c8517ff81
twistyn <tnew2294@gmail.com>
2015-05-12 11:10:55 +1000

    Readded index on country codes

---
+++ rails/db/migrate/20150512010811_re_add_index_on_country_codes.rb
@@ -0,0 +1,6 @@
+class ReAddIndexOnCountryCodes < ActiveRecord::Migration
+  def change
+    add_index :countries, :country_code_2, :unique => true
+    add_index :countries, :country_code_3, :unique => true
+  end
+end
