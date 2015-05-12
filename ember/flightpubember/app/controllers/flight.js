60faa6606b2c4f0c008ba3998226e038d34a7826
Alistair Woodcock <alistair@imagomedia.com.au>
2015-05-13 01:32:33 +1000

    accordian animation working

--- ember/flightpubember/app/controllers/flight.js
+++ ember/flightpubember/app/controllers/flight.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+	needs: ['flights'],
+
 	timeBarStyle: Ember.computed('timeBarWidth', 'timeBarLeft', function() {
 		var width = 'width:20%;';
 		var left = 'left:50%;';
@@ -12,13 +14,37 @@ export default Ember.ObjectController.extend({
 
 	actions: {
 		selected: function(){
+			var flightsController = this.get('controllers.flights');
+
+			var selectedFlight = flightsController.get('selected');
+
+
+			var flightID = '#' + this.get('flightNumber');
+			
 			if(this.get('selected'))
 			{
 				this.set('selected', false);
+
+				flightsController.set('selected', null);
+				
+				$(flightID).animate({height: 1}, 110, function(){
+					$(flightID).css({'display':'none'});
+				});
+
 			}
 			else
 			{
 				this.set('selected', true);
+				
+				if(selectedFlight != null)
+				{
+					selectedFlight.send('selected'); //toggles to deselected
+				}
+
+				flightsController.set('selected', this);
+
+				$(flightID).css({'display':'block'});
+				$(flightID).animate({height: 110}, 110);
 			}
 		},
 	}
