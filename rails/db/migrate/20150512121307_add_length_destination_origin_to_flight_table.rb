8f97222a5467c9e286f4e222c182fe85cbed58a4
msikkema <mattsikkema@icloud.com>
2015-05-12 23:35:29 +1000

    removed nonworking migration

--- rails/db/migrate/20150512121307_add_length_destination_origin_to_flight_table.rb
+++
@@ -1,9 +0,0 @@
-class AddLengthDestinationOriginToFlightTable < ActiveRecord::Migration
-  def change
-    change_table :flights do |t|
-      t.integer :trip_length
-      t.string :destination
-      t.string :origin
-    end
-  end
-end
