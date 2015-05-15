56f858398604d38ec57d76e3e54dd934088ccac7
msikkema <mattsikkema@icloud.com>
2015-05-15 15:58:57 +1000

    Some sample data added with the new fields.

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -9,33 +9,93 @@
 # Create some basic flight seeds for the API testing stuff
 # Replace this later with a flights fixture thang
 # NOTE: fields that need to be null in the table should not have an entry in the key/value pairs.
-#Flight.create(flight_number:            'AAA111',
-#              price:                    '8456',
-#              seatsAvailable:           '10',
-#              departureTime:            '2400',
-#              arrivalTime:              '1300',
-#              trip_length:              '5500',
-#              destination:              'Sydney',
-#              origin:                   'Tokyo',
-#              has_stopover:             false)
-##              stopover_arrival_time:    null,
-##              stopover_departure_time:  null,
-##              stopover_destination:     null,
-##              stopover_origin:          null)
-#
-#Flight.create(flight_number:            'AAA112',
-#              price:                    '9000',
-#              seatsAvailable:           '11',
-#              departureTime:            '2300',
-#              arrivalTime:              '1500',
-#              trip_length:              '7500',
-#              destination:              'Tokyo',
-#              origin:                   'Sydney',
-#              has_stopover:             'true',)
-#              stopover_arrival_time:    '2200',
-#              stopover_departure_time:  '2300',
-#              stopover_destination:     'Darwin',
-#              stopover_origin:          'Sydney')
+Flight.create(
+			  id: 						'1',
+			  flight_number:            'AAA111',
+              price:                    '8456',
+              seatsAvailable:           '10',
+              departureTime:            '2400',
+              arrivalTime:              '1300',
+              trip_length:              '5500',
+              destination:              'Newcastle',
+              origin:                   'Sydney',
+              is_composite_flight:      false)
+
+Flight.create(
+			  id: 						'2',
+			  flight_number:            'AAA111',
+              price:                    '8456',
+              seatsAvailable:           '10',
+              departureTime:            '1400',
+              arrivalTime:              '1700',
+              trip_length:              '5500',
+              destination:              'Darwin',
+              origin:                   'Newcastle',
+              is_composite_flight:      false)	
+			  
+Flight.create(
+			  id: 						'3',
+			  flight_number:            'AAA111',
+              price:                    '8456',
+              seatsAvailable:           '10',
+              departureTime:            '2400',
+              arrivalTime:              '1700',
+              trip_length:              '5500',
+              destination:              'Darwin',
+              origin:                   'Sydney',
+              is_composite_flight:      true,
+			  leg_1_id:					'1',
+			  leg_2_id:					'2')	
+			  
+Flight.create(
+			  id: 						'4',
+			  flight_number:            'BBB111',
+              price:                    '8456',
+              seatsAvailable:           '10',
+              departureTime:            '2400',
+              arrivalTime:              '1300',
+              trip_length:              '5500',
+              destination:              'Jakarta',
+              origin:                   'Darwin',
+              is_composite_flight:      false)
+
+Flight.create(
+			  id: 						'5',
+			  flight_number:            'BBB111',
+              price:                    '8456',
+              seatsAvailable:           '10',
+              departureTime:            '1400',
+              arrivalTime:              '1700',
+              trip_length:              '5500',
+              destination:              'Surabaya',
+              origin:                   'Jakarta',
+              is_composite_flight:      false)	
+			  
+Flight.create(
+			  id: 						'6',
+			  flight_number:            'BBB111',
+              price:                    '8456',
+              seatsAvailable:           '10',
+              departureTime:            '2400',
+              arrivalTime:              '1700',
+              trip_length:              '5500',
+              destination:              'Surabaya',
+              origin:                   'Darwin',
+              is_composite_flight:      true,
+			  leg_1_id:					'4',
+			  leg_2_id:					'5')		
+			  
+Flight.create(
+			  id: 						'7',
+			  flight_number:            'BBB112',
+              price:                    '8456',
+              seatsAvailable:           '10',
+              departureTime:            '2400',
+              arrivalTime:              '1700',
+              trip_length:              '5500',
+              destination:              'Newcastle',
+              origin:                   'Rome',
+              is_composite_flight:      false)				  	  		  	
 
 
 # To specify our own Fixtures, use a thing similar to below, 
