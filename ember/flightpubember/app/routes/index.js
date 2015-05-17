40d0292d5a0b4cdbbb77c80b97b5c935a85e09ac
Alistair Woodcock <alistair@imagomedia.com.au>
2015-05-17 13:23:16 +1000

    emberfying the search form

--- ember/flightpubember/app/routes/index.js
+++ ember/flightpubember/app/routes/index.js
@@ -9,6 +9,15 @@ export default Ember.Route.extend({
      setupController: function(controller, model) {
      	controller.set('content', model);
      	controller.set('tickets', this.store.find('ticketClass'));
+     	
+     	controller.set('people', 
+     		[   {number: "1"},
+     			{number: "2"},
+     			{number: "3"},
+     			{number: "4"},
+     			{number: "5"},
+     			{number: "6"}
+  			])
      }
 
 });
