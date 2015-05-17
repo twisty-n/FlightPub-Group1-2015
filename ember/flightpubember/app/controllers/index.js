ba614ab0cfc0df1b38186fc0b0e31ee3ccd67cfa
Alistair Woodcock <alistair@imagomedia.com.au>
2015-05-17 15:18:58 +1000

    filtering destinations working mostly

--- ember/flightpubember/app/controllers/index.js
+++ ember/flightpubember/app/controllers/index.js
@@ -1,12 +1,79 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  
+
+
+//these are the same destination search but for different inputs
+// I'm doing it this way to quickly move on to other stuff, and since
+// calling functions within controllers makes it hard to refactor, but
+// it's possible. 
+  toDestinationSearch: function(){
+  		var filter = this.get('to');
+  		if(filter == undefined || (/\S/.test(filter)) )
+  		{
+  			filter = '';
+  		}
+
+  		var result = this.get('destinations').filter(function(item, index, enumerable){
+  			var stringToMatch = '';
+  				stringToMatch += item.get('destinationCode') || ''; 
+	  			stringToMatch += item.get('countryName') || '';
+	  			stringToMatch += item.get('alternateName1') || '';
+	  			stringToMatch += item.get('countryCode3') || '';
+	  			stringToMatch += item.get('countryCode2') || '';
+	  			stringToMatch += item.get('airport') || '';
+
+  			return stringToMatch.toLowerCase().match(filter.toLowerCase());
+		});
+
+		console.log(result);
+		return result;
+
+  }.property('to'),
+
+  fromDestinationSearch: function(){
+  		var filter = this.get('from');
+  		if(filter == undefined || (/\S/.test(filter)) )
+  		{
+  			filter = '';
+  		}
+		
+		var result = this.get('destinations').filter(function(item, index, enumerable){
+  			var stringToMatch = '';
+  				stringToMatch += item.get('destinationCode') || '';
+	  			stringToMatch += item.get('countryName') || '';
+	  			stringToMatch += item.get('alternateName1') || '';
+	  			stringToMatch += item.get('countryCode3') || '';
+	  			stringToMatch += item.get('countryCode2') || '';
+	  			stringToMatch += item.get('airport') || '';
+
+	  			return stringToMatch.toLowerCase().match(filter.toLowerCase());
+		});
+
+		if(result.length == 0)
+		{
+			$("#left-box .drop-down").hide();
+			$("#left-box .arrow").hide();
+		}
+		else
+		{
+			$("#left-box .drop-down").show();
+			$("#left-box .arrow").show();
+		}
+		console.log(result);
+		return result;
+
+  }.property('from'),
+
   actions: {
   	flightSearch: function(){
   		console.log("no good, we're controlling it from the wrong controller :/");
   		console.log("but don't worry this means it works");
-  	}
+  	},
+
+  	setFrom: function(value){
+  		console.log("gotta set from value to "+value);
+  	},
   }
   
 });
--- ember/flightpubember/app/routes/index.js
+++ ember/flightpubember/app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     setupController: function(controller, model) {
-    	controller.set('dest', this.store.find('destination'));
+    	controller.set('destinations', this.store.find('destination'));
      	controller.set('content', model);
      	controller.set('tickets', this.store.find('ticketClass'));
      	controller.set('people', [{number: "1"},{number: "2"},{number: "3"},{number: "4"},{number: "5"},{number: "6"}]);
