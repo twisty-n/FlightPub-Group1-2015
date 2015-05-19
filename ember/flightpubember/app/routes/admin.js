867db73ae106bb72b124b72e79606d734d228884
Alistair Woodcock <alistair@imagomedia.com.au>
2015-05-20 01:39:47 +1000

    starting to try and get data

--- ember/flightpubember/app/routes/admin.js
+++ ember/flightpubember/app/routes/admin.js
@@ -5,5 +5,7 @@ import AuthenticatedRoute from 'flightpubember/routes/authenticated'
 export default AuthenticatedRoute.extend({
     setupController: function(controller, model) {
      controller.set('model', this.store.find('user'));
+     //controller.set('airlines', this.store.find('airlines'));
+     //controller.set('users', this.store.find('users'));
     }
 });
