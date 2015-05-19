cc334819bace04eaba13f9ab53a53db973f960bc
msikkema <mattsikkema@icloud.com>
2015-05-19 11:32:01 +1000

    Continuing work on the bfs

--- rails/app/search/flight_search.rb
+++ rails/app/search/flight_search.rb
@@ -1,6 +1,9 @@
 # This class performs the search functionality in the assignment. It interfaces with the search
 # API. To alter the search algorithm, replace this file, and don't touch anything else.
-class FlightSearch < ActiveRecord::Base
+
+require 'errors/invalid_input_error'
+	
+class FlightSearch
 
 # This is the basic algorithm, implemented in ruby and not MySQL. 
 # It lacks important parameters atm (like times). It works like this:
@@ -31,22 +34,43 @@ output_on = true		#Make true
 
 	# This is a test method
 	def self.speak
-		'This is a string from the search class'
+		'This is a string from the search classss'
 	end
 	
 	
 	# This method does a breadth first search of the flights table.
 	# It will return an array of path objects that match
-	def self.search
+	def self.outputFlightsTable
 		raw_flights = Flight.all
 		
-		puts "Converting flight activerecord associations to hash..."
-		hash_flights = raw_flights.as_json
-		puts "Preparing to print out the newly created hash..."
-		puts hash_flights
+		puts "Converting flight activerecord associations to array of hash browns..."
+		array_of_hash_flights = raw_flights.as_json
+		puts "\n THE FLIGHT VALUES IN THE DATABASE ARE:\N"
 		
+		array_of_hash_flights.each do |value|
+			
+			# Load the flight from the array
+			flight = value
+			# Print out details of the flight
+			flight.each do |k,v|
+				puts k.to_s + ":		" + v.to_s
+			end
+			puts "\n ---------------------------------------------------- \n"
+		end
+	puts "\nEND OF FLIGHT VALUES\n"
 	
 	end	#eof search function
 	
+	
+	# This is the main search method
+	def self.search(target)
+	
+		# verify the input is not blank
+		if target == nill || target.blank
+			raise InvalidInputError "The input is invalid"
+		
+		end
+	
+	end	#eof search function
 	
 end
