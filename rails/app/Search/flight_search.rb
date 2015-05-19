bf21491bd0dfe2a5874aede5e8cc0fc617c82c31
Tristan Newmann <tristan_dev@tristans-mbp.newcastle.edu.au>
2015-05-19 16:43:52 +1000

    Deleted search folder to fix naming issues

--- rails/app/Search/flight_search.rb
+++
@@ -1,6 +0,0 @@
-# This class performs the search functionality in the assignment. It interfaces with the search
-# API. To alter the search algorithm, replace this file, and don't touch anything else.
-
-class FlightSearch
-
-end
