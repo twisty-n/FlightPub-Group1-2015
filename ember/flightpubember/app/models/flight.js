ae1f8acf021f5f853acaf4faa16eccf5d45aa7bf
twistyn <tnew2294@gmail.com>
2015-05-21 00:33:36 +1000

    Chnaged from number to date

--- ember/flightpubember/app/models/flight.js
+++ ember/flightpubember/app/models/flight.js
@@ -3,8 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     flightNumber: 		DS.attr('string'),
     price:              DS.attr('number'),
-    departureTime:      DS.attr('number'),
-    arrivalTime:        DS.attr('number'), //number for now, we probably want date-time
+    departureTime:      DS.attr('date'),
+    arrivalTime:        DS.attr('date'), //number for now, we probably want date-time
     seatsAvailable:     DS.attr('number'),
   
     /*legs: DS.attr('array') I'm hoping this is a thing, we need to get all the legs of the flight*/
