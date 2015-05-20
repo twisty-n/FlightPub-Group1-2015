b21687da688676a3927172505b4a07f8ba4ff743
twistyn <tnew2294@gmail.com>
2015-05-20 16:17:12 +1000

    Got all associations working correctly between flights and legs

--- rails/app/models/flight.rb
+++ rails/app/models/flight.rb
@@ -1,10 +1,13 @@
 class Flight < ActiveRecord::Base
 
     belongs_to :airline
-    belongs_to :ticket_availability
     belongs_to :destination, class_name: "Destination"
     belongs_to :origin, class_name: "Destination"
 
+    belongs_to :leg_1, class_name: "Flight", foreign_key: "leg_1_id"
+    belongs_to :leg_2, class_name: "Flight", foreign_key: "leg_2_id"
+
+    has_many :ticket_availabilities
     has_many :saved_flights
     has_many :users, :through => :saved_flights
 
