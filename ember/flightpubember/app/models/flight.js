b430f125c1e3bfc59669dce30ebb9fd2bb69ebc2
Alistair Woodcock <alistair@imagomedia.com.au>
2015-05-20 23:57:42 +1000

    preliminary flights width stretching

--- ember/flightpubember/app/models/flight.js
+++ ember/flightpubember/app/models/flight.js
@@ -6,15 +6,6 @@ export default DS.Model.extend({
     departureTime:      DS.attr('number'),
     arrivalTime:        DS.attr('number'), //number for now, we probably want date-time
     seatsAvailable:     DS.attr('number'),
-  
-    /*legs: DS.attr('array') I'm hoping this is a thing, we need to get all the legs of the flight*/
-});
+  	tripLength: 		DS.attr('number'),
 
-/*
-let Flight = DS.Model.extend({
-	flightNumber: 		DS.attr('string'),
-    price:              DS.attr('string'),
 });
-
-export default Flight;
-*/
