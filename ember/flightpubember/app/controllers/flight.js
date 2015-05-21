288957532d410c6adbcda6ad8682c71417bc522f
Alistair Woodcock <alistair@imagomedia.com.au>
2015-05-21 17:03:37 +1000

    playing with flight times representation

--- ember/flightpubember/app/controllers/flight.js
+++ ember/flightpubember/app/controllers/flight.js
@@ -95,45 +95,19 @@ export default Ember.ObjectController.extend({
 		var lengthRatio = flightTime/avgLength;
 
 		var widthPercent = 100;
-		var widthChange = 0;
 
 
-
-		if(lengthRatio < 0.1)
-		{
-			widthChange = (lengthRatio);
-		}
-		else if(lengthRatio < 1)
-		{
-			widthChange = (lengthRatio);
-		}
-		else
-		{
-			widthChange = (lengthRatio);
-		}
-
-		if(widthChange > 1)
-		{
-			widthChange = Math.log10(lengthRatio);
-		}
-		else
-		{
-			widthChange += 0.2; 
-		}
+		widthPercent *= Math.log10(lengthRatio+1);
+		widthPercent += Math.pow(10, Math.log10(lengthRatio)); 
+		//widthPercent += 10; 
+		
 
 
-		widthPercent *= widthChange;
+		//widthPercent *= widthChange;
 
 
-		var leftPercent = 8; // we have a base of 10 for style
+		var leftPercent = 8; // we have a base of 8 for style
 
-
-		//var departureTime = this.get('departureTime');
-		//var departurePercentage = departureTime/2400; //this is just using the current data, not correct at all
-
-		// console.log(departurePercentage);
-
-		//leftPercent += 0;//10*departurePercentage;
 
 		return "width:"+widthPercent+"%; left: "+leftPercent+"%;";
 
