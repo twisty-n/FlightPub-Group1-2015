05f22a0c48d031b49a03c441f993844929d65f5d
twistyn <tnew2294@gmail.com>
2015-05-28 02:05:39 +1000

    Promotion applying fonr-end complete

--- ember/flightpubember/app/controllers/admin.js
+++ ember/flightpubember/app/controllers/admin.js
@@ -1,6 +1,68 @@
 import Ember from 'ember';
 
 
-export default Ember.ObjectController.extend({
-    content: {}	
+export default Ember.Controller.extend({
+    content: {},
+    promoFlightList: null,
+    discountAmount: 0,
+    actions: {
+
+        loadPromoFlights: function() {
+
+            Ember.$("#promo-search").hide();
+            Ember.$("#promo-select").show();
+
+            var searchFields =  this.getProperties('flightNumberPromo', 
+                'departDatePromo', 'maxDepartDatePromo');
+            searchFields['q_type'] = 'promo';
+
+            this.set('promoFlightList', 
+                this.store.findQuery('flight', searchFields));
+            console.log(this.get('promoFlightList'));
+
+
+        },
+
+        cancelPromoSelect: function() {
+
+            // Invalidate the data here
+            // 
+            // Then display the normal form
+            Ember.$("#promo-search").show();
+            Ember.$("#promo-select").hide();
+        },
+
+        applyPromotion: function(flight) {
+
+            var promo_flight = flight;
+            data = {
+                flight_id: promo_flight.id,
+                discount: this.get('discountAmount')
+            }
+            Ember.$.post('api/apply_promotion', data).then(function(response) {
+
+                // Remove the record from the store
+                promo_flight.unloadRecord();
+
+            }, function(error) {
+                alert('Unable to apply promotion to flight ' + promo_flight.id);
+            });
+
+        },
+
+        setupDiscount: function() {
+
+            var amount = parseInt(this.get('discount'));
+            console.log(typeof amount);
+            if ( typeof amount != 'number' ) {
+                alert('Discount is not a valid price');
+                this.set('discount', "");
+                return;
+            }
+
+            this.set('discountAmount', amount);
+
+        }
+
+    }
 });
