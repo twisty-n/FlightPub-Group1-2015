5942b00189a0fc4725bf5d71bc488758bd564c6f
twistyn <tnew2294@gmail.com>
2015-05-27 14:07:18 +1000

    Flight invoicing emailing workign

--- rails/app/mailers/purchase_mailer.rb
+++ rails/app/mailers/purchase_mailer.rb
@@ -3,15 +3,15 @@ class PurchaseMailer < ApplicationMailer
     default from: 'noreply@flightpub.com'
 
     # Defines an email to be sent to a users email address when they register for a flightpub account
-    def purchase_flight_email(dep_journey, ret_journey, user_id)
+    def purchase_flight_email(dep_journey_id, ret_journey_id, user_id)
 
         @user = User.find_by(id: user_id)
 
         @dep_flights = Array.new
         @ret_flights = Array.new
 
-        dep_journey = Journey.find_by(id: dep_journey)
-        ret_journey = Journey.find_by(id: ret_journey)
+        dep_journey = Journey.find_by(id: dep_journey_id)
+        ret_journey = Journey.find_by(id: ret_journey_id)
 
         dep_journey.journey_maps.each do |map|
             @dep_flights.push( {
@@ -20,11 +20,13 @@ class PurchaseMailer < ApplicationMailer
                 } )
         end
 
-        ret_journey.journey_maps.each do |map|
-            @ret_flights.push( {
-                    'order' => map.order_in_journey,
-                    'flight'=> map.flight
-                } )
+        if ret_journey != nil
+            ret_journey.journey_maps.each do |map|
+                @ret_flights.push( {
+                        'order' => map.order_in_journey,
+                        'flight'=> map.flight
+                    } )
+            end
         end
 
         mail(to: @user.email, subject: 'Your flight Itinerary')
@@ -34,7 +36,29 @@ class PurchaseMailer < ApplicationMailer
 
     def generate_invoice(dep_tickets, ret_tickets, num_to_get, user_id)
 
-        print 'Yay! we have an invoice!'
+        @user = User.find_by(id: user_id)
 
+        @dep_tickets = Array.new
+        @ret_tickets = Array.new
+
+        @num_to_get = num_to_get
+
+        @sum_dep = 0
+        @sum_ret = 0
+
+        dep_tickets.each do |ticket_id|
+            tick = TicketAvailability.find_by(id: ticket_id)
+            @dep_tickets.push( tick )
+            @sum_dep += tick.price
+        end
+
+        if ret_tickets != nil
+            ret_tickets.each do |ticket_id|
+                tick = TicketAvailability.find_by(id: ticket_id)
+                @ret_tickets.push( tick )
+                @sum_ret += tick.price
+            end
+        end
+        mail(to: @user.email, subject: 'Invoice details for your recent purchase')
     end
 end
