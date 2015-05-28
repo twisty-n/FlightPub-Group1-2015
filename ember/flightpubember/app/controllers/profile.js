821d3260ee2d97ba1d8cf8fa6f421ded9eaa0fd6
Alistair Woodcock <alistair@imagomedia.com.au>
2015-05-28 15:38:29 +1000

    it all looks gross but whatever

--- ember/flightpubember/app/controllers/profile.js
+++ ember/flightpubember/app/controllers/profile.js
@@ -5,29 +5,82 @@ export default Ember.ObjectController.extend({
 
     needs: ['application'],
 
+    //we can separate these into a single
+    // purchased flight array and then return the results from there
+    // would reduce code reuse
     upcomingJourneys: function(){
+        function parseDate(input) {
+            var parts = input.match(/(\d+)/g);
+            // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+            return new Date(parts[0], parts[1]-1, parts[2], parts[3], parts[4], parts[5]); // months are 0-based
+        }
+
         var journeys = this.get('model.journeys');
 
         var upcoming = Ember.A([]); 
 
         journeys.forEach(function(journey)
         {
-            upcoming.pushObject(journey);
-        });
+            var j = journey.journey.journey;
+            
+            if(journey.save_type === 'purchased_flight' 
+            && parseDate(j.departure_time) > new Date())
+            {
 
+                j.hasMultipleFlights = false;
+                j.formatted
+
+                if(j.flights.length > 1)
+                {
+                    j.hasMultipleFlights = true;
+                }
+
+                upcoming.pushObject(j);
+            }
+        });
+        console.log('Upcoming: ');
+        console.log(upcoming);
         return upcoming;
     }.property('model.journeys'),
 
     pastJourneys: function(){
-        
-        return 'OY';
+        function parseDate(input) {
+            var parts = input.match(/(\d+)/g);
+            // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
+            return new Date(parts[0], parts[1]-1, parts[2], parts[3], parts[4], parts[5]); // months are 0-based
+        }
+
+        var journeys = this.get('model.journeys');
+
+        var past = Ember.A([]); 
+
+        journeys.forEach(function(journey)
+        {
+            if(journey.save_type === 'purchased_flight' 
+            && parseDate(journey.journey.journey.departure_time) <= new Date())
+            {
+                past.pushObject(journey.journey.journey);
+            }
+        });
+        console.log('Past: ');
+        console.log(past);
+        return past;
     }.property('model.journeys'),
 
     savedJourneys: function(){
-        
+        var journeys = this.get('model.journeys');
 
-        return 'Y O O O';
+        var saved = Ember.A([]); 
 
+        journeys.forEach(function(journey)
+        {
+            if(journey.save_type === 'purchased_flight')
+            {
+                saved.pushObject(journey.journey.journey);
+            }
+        });
+
+        return saved;
     }.property('model.journeys'),
 
     actions: {
@@ -82,6 +135,36 @@ export default Ember.ObjectController.extend({
             });
             
 
+        },
+
+        showSettings: function(){
+            Ember.$("#purchased-flights").hide();
+            Ember.$("#saved-flights").hide();
+            Ember.$("#settings").show();
+            this.send('setActive', '#settings-switch');
+        },
+
+        showSaved: function(){
+
+            Ember.$("#purchased-flights").hide();
+            Ember.$("#saved-flights").show();
+            Ember.$("#settings").hide();
+            this.send('setActive', '#saved-trips-switch');
+        },
+
+        showPurchased: function(){
+            Ember.$("#purchased-flights").show();
+            Ember.$("#saved-flights").hide();
+            Ember.$("#settings").hide();
+            this.send('setActive', '#purchased-trips-switch');
+        },
+
+        setActive: function(activeID){
+            Ember.$("#purchased-trips-switch").removeClass("user-nav-active");
+            Ember.$("#saved-trips-switch").removeClass("user-nav-active");
+            Ember.$("#settings-switch").removeClass("user-nav-active");
+
+            Ember.$(activeID).addClass("user-nav-active");
         }
 
 
