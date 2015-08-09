314c6c9432be473975b43fba4de7d10787849232
Tristan Newmann <tristan_dev@Tristans-MacBook-Pro.local>
2015-08-09 21:42:20 +1000

    added the MessagesController

---
+++ rails/app/controllers/api/messaging_controller.rb
@@ -0,0 +1,31 @@
+class Api::MessagingController < ApplicationController
+
+=begin
+
+	Responsible for handling messages and conversations between users
+	This includes the rub_shoulders functionality and any other additional 
+	messaging functionality
+
+=end
+
+	
+	# Hook to send a message to a user
+	def post_message
+
+		# Define the accepted params
+
+	end
+
+
+	# Hook to rub shoulders with another user
+	def rub_shoulders
+
+	end
+
+	# Hook to delete a conversation
+	def delete_conversation
+
+	end
+
+	
+end
